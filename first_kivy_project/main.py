569c32e3700abf349105a31be83b0bdb809008d5
Sijango <klimkin77@gmail.com>
2021-04-03 15:48:19 +0300

    add two kivy projects

---
+++ first_kivy_project/main.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+
+
+class WormApp(App):
+    def build(self):
+        self.but = Button()
+        self.but.pos = (100, 100)
+        self.but.size = (200, 200)
+        self.but.text = "Hello, cruel world"
+
+        self.form = Widget()
+        self.form.add_widget(self.but)
+        return self.form
+
+
+if __name__ == '__main__':
+    WormApp().run()
